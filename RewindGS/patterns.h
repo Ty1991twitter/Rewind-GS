f99040f9412ace3959ed97111ba88babc568bf53
Ty_1991 <shayeedwarner@gmail.com>
2022-10-30 23:13:27 +0000

    everything

---
+++ RewindGS/patterns.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <string>
+#include <minwindef.h>
+namespace FNPatterns {
+	std::string GObjects = "";
+}
+void FindPattern(std::string pattern) {
+	//TODO : The whole damn function.
+    // I forgot how to do this lmfao.
+}
